0f1e402379c3ba5ab2f17e58597f45d55baa8501
Paul Duvall <duvallpj@gmail.com>
2018-12-20 11:51:08 +0000

    Update of canvas-animated3.js

--- canvas-animated3.js
+++ canvas-animated3.js
@@ -1,3 +1,5 @@
+// Animated square that moves around and changes size
+
 var canvas = document.querySelector('canvas');
 
 // Ensure the canvas takes up the full browser window
@@ -6,64 +8,14 @@ canvas.height = window.innerHeight;
 
 // Create the context variable
 const c = canvas.getContext('2d');
-
-// Create a Rectangle object
-function Rectangle(x, y, width, height, r, g, b) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.r = r;
-  this.g = g;
-  this.b = b;
-
-  this.draw = function() {
-    // Draws the Rectangle
-    c.fillRect(this.x, this.y, this.width, this.height);
-    
-    // Set the colour
-    c.fillStyle = `rgba(${this.r}, ${this.g}, ${this.b}, 1)`;
-    // c.stroke();
-  }
-
-  this.update = function() {
-    // Updated the position of the circle
-    // Changes x-coordinate when circle hits left or right of viewport
-    // if(this.x + 30 > innerWidth || this.x - this.width < 0) {
-    // // Change velocity direction
-    // } 
-
-    // // Changes y-coordinate when circle hits left or right of viewport
-    // if(this.y + 30 > innerHeight || this.y - this.width < 0) {
-    // // Change velocity direction
-    // }
-  
-    // this.x += this.dx;
-    // this.y += this.dy;
-
-    this.draw();
-  }
-}
-
-// Array to contain the circles
-let rectArray = [];
-
-c.lineWidth = 3;
-// c.strokeStyle = 'rgba(84, 133, 207, 1)';
-
-// Generates the circles and adds them to the circleArray
-for(var i = 0; i < 10; i++) {
-  
-  let x = Math.random() * (innerWidth - radius * 2) + radius;
-  let y = Math.random() * (innerHeight - radius * 2) + radius;
-  let width = 200;
-  let height = 200;
-  var r = Math.floor(Math.random() * 255);
-  var g = Math.floor(Math.random() * 255);
-  var b = Math.floor(Math.random() * 255);
-  
-  rectArray.push(new Rectangle(x, y, width, height, r, g, b));
-}
+x = 200;
+y = 200;
+width = 100;
+height = 100;
+let dx = 3;
+let dy = 3;
+let dWidth = 1;
+let dHeight = 1;
 
 function animate() {
   // Creates recursion
@@ -71,9 +23,35 @@ function animate() {
   // Clears the canvas for each iteration
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  for (var i = 0; i < rectArray.length; i++){
-    rectArray[i].update();
-  }
+  // Draw square
+  c.fillStyle = 'rgba(128, 164, 219, 1)';
+  c.fillRect(x, y, width, height);
+
+    // Changes x-coordinate when circle hits left or right of viewport
+    if(x > innerWidth - 100 || x < 0) {
+      dx = -dx;
+    } 
+  
+    // Changes y-coordinate when circle hits left or right of viewport
+    if(y > innerHeight - 100 || y < 0) {
+      dy = -dy;
+    }
+
+    // Changes width when it reaches 200
+    if(width > 150 || width < 50) {
+      dWidth = -dWidth;
+    } 
+  
+    // Changes width when it reaches 200
+    if(height > 150 || height < 50) {
+      dHeight = -dHeight;
+    }
+  
+  x += dx;
+  y += dy;
+  width += dWidth;
+  height += dHeight;
+
 }
 
 animate();
